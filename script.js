522486e09f2faff3eedc3cee8411bdee58173ecd
alisina shakibafar <alisina.shakibafar@gmail.com>
2025-02-06 17:13:24 +0330

    Saved

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-const onLine=navigator.onLine;
-if (!onLine) {
-    function onlined() {
-        
-    }
+const online = navigator.onLine;
+const lineh=document.getElementById("");
+if (!online) {
+    onofflined();
+}else{
+    ononlined();
 }
 
+function onofflined() {
+    
+}
+
+function ononlined() {
+    
+}
+
+
 setInterval(() => {
-    onLine=navigator.onLine;
-    onlined();
-},1000);
+    onLine = navigator.onLine;
+    if (!online) {
+        onofflined();
+    }else{
+        ononlined();
+    }
+}, 1000);
